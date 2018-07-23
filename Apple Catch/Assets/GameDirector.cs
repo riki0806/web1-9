c716f8f175d5d39d09e05bd0fd6da7894b977f92
riki0806 <michitaka0806@gmail.com>
2018-07-23 18:01:03 +0900

    update2

--- Apple Catch/Assets/GameDirector.cs
+++ Apple Catch/Assets/GameDirector.cs
@@ -17,7 +17,7 @@ public class GameDirector : MonoBehaviour {
     }
     public void GetBomb()
     {
-        this.point -= 30;
+        this.point -= 20;
     }
     // Use this for initialization
     void Start () {
@@ -33,6 +33,7 @@ public class GameDirector : MonoBehaviour {
         if (this.time < 0 || this.point < 1) 
         {
             this.time = 0;
+            this.point = 0;
             this.generator.GetComponent<ItemGenerator>().SetParameter(10000.0f, 0, 0);
         }
         else if (0 <= this.time && this.time < 5)
@@ -41,18 +42,18 @@ public class GameDirector : MonoBehaviour {
         }
         else if (5 <= this.time && this.time < 10)
         {
-            this.generator.GetComponent<ItemGenerator>().SetParameter(0.4f, -0.06f, 3);
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.4f, -0.06f, 4);
         }
         else if (10 <= this.time && this.time < 20)
         {
-            this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.05f, 6);
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.05f, 7);
         }
         else if (20 <= this.time && this.time < 30)
         {
-            this.generator.GetComponent<ItemGenerator>().SetParameter(0.9f, -0.02f, 6);
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.04f, 8);
         }
 
         this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
-        this.pointText.GetComponent<Text>().text = this.point.ToString()+" point";
+        this.pointText.GetComponent<Text>().text = "HP: "+this.point.ToString();
     }
 }
